9100fa4694da16a830ede4c98b6a31983323a46a
daoducnha <daoducnha29493@gmail.com>
2021-04-04 13:46:08 +0700

    Implement Status component

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
+import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Status from './components/Status';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  renderMessageList() {
+    return (
+      <View style={styles.content}></View>
+    )
+  }
+
+  renderInputMethod() {
+    return (
+      <View style={styles.inputMethodEditor}></View>
+    )
+  }
+
+  renderToolBar() {
+    return (
+      <View style={styles.toolBar}></View>
+    )
+  }
+  render() {
+    return (
+      <View style={styles.container} >
+        <Status />
+        {this.renderMessageList()}
+        {this.renderToolBar()}
+        {this.renderInputMethod()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolBar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white',
   },
 });
